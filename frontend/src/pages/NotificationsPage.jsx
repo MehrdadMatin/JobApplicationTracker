d83011a7680ed93d2fc508db1869db97833be728
climbTheStairs <yxtqwf+github@gmail.com>
2025-09-14 14:03:29 -0700

    frontend: notificationspage: mk it look pretty

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -1,15 +1,17 @@
+import "./NotificationsPage.css"
 import {useState, useEffect} from "react"
 export default () => {
 	const [state, setState] = useState([])
 	useEffect(() => {
 		fetch("/api/notifications").then(resp => resp.json()).then(setState)
 	}, [])
-	return (<>
+	return (<div className="page-container">
 		<h1>Notifications</h1>
 		<ul>
 			{state.map(({message, pub_date}, i) => <li key={i}>
-				{pub_date}: {message}
+				<div className="ts">{pub_date}</div>
+				<div className="msg">{message}</div>
 			</li>)}
 		</ul>
-	</>)
+	</div>)
 }
